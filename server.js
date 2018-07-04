19815a01c1122f13d23ab47fa7ec311eb215698c
yuvalbl <yuval.bar.levi@gmail.com>
2018-07-04 13:19:55 +0300

    add routes

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const db = require('./db');
 const express = require('express');
 const app = express();
+const router = express.Router();
+app.use(express.json());
 
-app.get('/api/robots', (req, res) => {
-  res.send(JSON.stringify(db.robots))
-});
+router.route('/parts')
+  .get((req, res) => {
+    res.send(JSON.stringify(db.parts))
+  });
 
-app.get('/api/parts', (req, res) => {
-  res.send(JSON.stringify(db.parts))
-});
+router.route('/robots')
+  .get((req, res) => {
+    res.send(JSON.stringify(db.parts))
+  });
+
+router.route('/robots/:id')
+  .get((req, res) => {
+    const id = parseInt(req.params.id);
+    const robot = db.robots.find(r => r.id === id);
+    res.send(JSON.stringify(robot));
+  })
+  .put((req, res) => {
+    const id = parseInt(req.params.id);
+    const index = db.robots.findIndex(post => post.id === id);
+    const item = {
+      id,
+      age: req.body.age,
+      name: req.body.name,
+    };
+    (index === -1) ? db.robots.push(item) : db[index] = item;
+    res.send(JSON.stringify(item));
+  })
+  .delete((req, res) => {
+    const id = parseInt(req.params.id);
+    const index  = db.findIndex(post => post.id === id);
+    if(index !== -1) {
+      db.robots.splice(index, 1);
+    }
+    res.send(JSON.stringify(db));
+  });
+
+
+app.use('/api', router);
 
 app.listen(3000,
   () => console.log('Robot labs API server listening on port 3000!')
